612607c106a14660cb003c1aa8c387621e041847
Aline Bury <alinebury@gmail.com.br>
2023-02-09 15:46:11 -0300

    :construction: Header and routes in progress

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img />
+    <nav>
+      <router-link to="/">Cards</router-link> |
+      <router-link to="/deckbuilder">Build A Deck</router-link> |
+      <router-link to="/battlerground">Battlegrounds</router-link> |
+      <router-link to="/mercenaries">Mercenaries</router-link> |
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "header"
+  }
+</script>
+
+<style>
+  
+</style>
